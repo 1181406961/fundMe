60739c3b3561c5018665521335d4bec8736644e7
1181406961 <1181406961@qq.com>
2022-10-20 10:42:54 +0800

    去掉无用注释

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,53 +1,35 @@
 // SPDX-License-Identifier: MIT
-// 1. Pragma
 pragma solidity ^0.8.7;
-// 2. Imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-// 3. Interfaces, Libraries, Contracts
-    error FundMe__NotOwner();
-
-/**@title A sample Funding Contract
- * @author Patrick Collins
- * @notice This contract is for creating a sample funding contract
- * @dev This implements price feeds as our library
- */
+error FundMe__NotOwner();
 contract FundMe {
-    // Type Declarations
     using PriceConverter for uint256;
 
-    // State variables
-    uint256 public constant MINIMUM_USD = 50 * 10**18;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     address private immutable i_owner;
     address[] private s_funders;
     mapping(address => uint256) private s_addressToAmountFunded;
     AggregatorV3Interface private s_priceFeed;
 
-    // Events (we have none!)
-
-    // Modifiers
     modifier onlyOwner() {
         // require(msg.sender == i_owner);
         if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
 
-    // Functions Order:
-    //// constructor
-    //// receive
-    //// fallback
-    //// external
-    //// public
-    //// internal
-    //// private
-    //// view / pure
-
     constructor(address priceFeed) {
         s_priceFeed = AggregatorV3Interface(priceFeed);
         i_owner = msg.sender;
     }
+    receive() external payable {
+        fund();
+    }
 
+    fallback() external payable {
+        fund();
+    }
     /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
         require(
@@ -71,7 +53,7 @@ contract FundMe {
         s_funders = new address[](0);
         // Transfer vs call vs Send
         // payable(msg.sender).transfer(address(this).balance);
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        (bool success,) = i_owner.call{value : address(this).balance}("");
         require(success);
     }
 
@@ -88,7 +70,7 @@ contract FundMe {
         }
         s_funders = new address[](0);
         // payable(msg.sender).transfer(address(this).balance);
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        (bool success,) = i_owner.call{value : address(this).balance}("");
         require(success);
     }
 
