78884e7db44d4e3abf853a9928ed222035dd60a9
1181406961 <1181406961@qq.com>
2022-10-20 10:43:27 +0800

    修改部署脚本

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,22 +1,31 @@
 const {network} = require("hardhat");
-const {developmentChains} = require("../helper-hardhat-config");
-const DECIMALS = "8"
-const INITIAL_PRICE = "200000000000" // 2000
-module.exports = async ({getNamedAccounts,deployments}) => {
+const {developmentChains} = require("../helper-hardhat-config")
+const DECIMALS = "8";
+const INITIAL_PRICE = "200000000000"; // 2000
+module.exports = async ({
+                            getNamedAccounts,
+                            deployments
+                        }) => {
     const {deploy, log} = deployments;
     const {deployer} = await getNamedAccounts();
-    console.log(`deployer is ${deployer}`)
+
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
-        await deploy("MockV3Aggregator",{
-            contract: "MockV3Aggregator",
-            from:deployer,
-            log:true,
-            args:[DECIMALS,INITIAL_PRICE], // from  chainlink source code
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
         })
         log("Mocks Deployed!");
-        log("-----------------------------------------");
+        log("------------------------------------------------");
+        log(
+            "You are deploying to a local network, you'll need a local network running to interact"
+        );
+        log(
+            "Please run `npx hardhat console` to interact with the deployed smart contracts!"
+        );
+        log("------------------------------------------------");
     }
 
-};
+}
 module.exports.tags = ["all", "mocks"]
--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,18 +1,15 @@
-// import
-// main function
-// call of main function
 const {network} = require("hardhat");
-const {developmentChains, networkConfig} = require("../helper-hardhat-config");
+const {networkConfig, developmentChains} = require("../helper-hardhat-config");
 const {verify} = require("../utils/verify")
-module.exports.default = async ({getNamedAccounts, deployments}) => {
+module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy, log} = deployments;
     const {deployer} = await getNamedAccounts();
-    const chainId = network.config.chainId
+    const chainId = network.config.chainId;
     let ethUsdPriceFeedAddress;
-    if (developmentChains.includes(network.name)) {
+    if (developmentChains.includes(network.name)){
         const ethUsdAggregator = await deployments.get("MockV3Aggregator");
         ethUsdPriceFeedAddress = ethUsdAggregator.address;
-    } else {
+    }else{
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
     }
     log("----------------------------------------------------")
@@ -21,9 +18,12 @@ module.exports.default = async ({getNamedAccounts, deployments}) => {
         from: deployer,
         args: [ethUsdPriceFeedAddress],
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
     log(`FundMe deployed at ${fundMe.address}`);
-    if (developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         await verify(fundMe.address, [ethUsdPriceFeedAddress]);
     }
-};
+
+}
+module.exports.tags = ["all", "fundme"]
